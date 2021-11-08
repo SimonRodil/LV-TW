46514e352d693770456a1660045044b32a7601b9
Simon <rodileduar.simon@gmail.com>
2021-11-07 23:36:04 -0400

    Porfile blade

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Auth::routes();
+# Ruta de Perfil
+Route::get('/profile', function () {
+    return view('auth.profile');
+})->middleware('auth');
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
